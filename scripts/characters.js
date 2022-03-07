7e4f7720599d8f036835da3c5928f4665af8d6e6
IamAdren <IamAdren@iamadrenn@gmail.com>
2022-03-06 19:33:46 -0500

    API Examples

--- scripts/characters.js
+++ scripts/characters.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 
-const API_URL = 'http://localhost:3001/v1';
+const API_URL = 'https://api.adrencad.com/v1';
 
-let key = 'Opw/cC0-ywpcJ2gafu.dw_IC7HauYjdurZYe_+faok';
+let key = 'API Key';
 
 axios.defaults.headers.common['Authorization'] = `Bearer ${key}`;
 
@@ -18,10 +18,51 @@ axios
 		console.log(data);
 	});
 
-// axios
-// 	.post(API_URL + '/characters/edit', {
-// 		characterId: '6219c8fd956198d240cfdcfb',
-// 	})
-// 	.then(({ data }) => {
-// 		console.log(data);
-// 	});
+axios
+	.post(API_URL + '/characters/create', {
+		// userId: '62101264e8bc434210fefd70',
+		// departmentId: '62101280e8bc434210fefd7a',
+		characterData: {
+			firstName: 'Bill',
+			middleInitial: 'L',
+			lastName: 'Booscavski',
+			alias: 'gansta',
+			residence: '109 gangster way',
+			DOB: '10-02-1995',
+			heightFeet: '6',
+			heightInches: '4',
+			weight: '180',
+			sex: 'Male',
+		},
+	})
+	.then(({ data }) => {
+		console.log(data);
+	});
+
+axios
+	.post(API_URL + '/characters/delete', {
+		characterId: '622550dc9c81332b08f90aab',
+	})
+	.then(({ data }) => {
+		console.log(data);
+	});
+
+axios
+	.post(API_URL + '/characters/edit', {
+		characterId: '622551cb7a33c94bc01fc95a',
+		updatedCharacter: {
+			firstName: 'asdasd',
+			middleInitial: 'L',
+			lastName: 'Booscavski',
+			alias: 'gansta',
+			residence: '109 gangster way',
+			DOB: '10-02-1995',
+			heightFeet: '6',
+			heightInches: '4',
+			weight: '180',
+			sex: 'Male',
+		},
+	})
+	.then(({ data }) => {
+		console.log(data);
+	});
